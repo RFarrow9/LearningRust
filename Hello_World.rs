78bfe9f2c93db461ed59371fee2c4a4abf0e8c96
Rob Farrow <rob.farrow@hotmail.com>
2019-03-11 20:58:16 +0000

    initial commit

---
+++ Hello_World.rs
@@ -0,0 +1,37 @@
+// Rust has to have a main function
+fn main() {
+
+    let answer = 43;
+    println!("Hello {}", answer);
+    assert_eq!(answer, 43);
+    for i in 0..7 {
+        //for printing the {} will be replaced by subsequent parameters
+        println!("Hello {}", i)
+    }
+    //checking conditionals
+    for i in 0..5 {
+        if i % 2 == 0 {
+            println!("even {}", i);
+        } else {
+            println!("odd {}", i);
+        }
+    }
+    for i in 0..5 {
+        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+        println!("{} {}", even_odd, i);
+    }
+    println!("test {} {}", 1, 1);
+    println!("{1}, {0}", "alice", "bob");
+    //formats the number as a binary string
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 226);
+    let mut sum = 0.0;
+    for i in 0..47 {
+        sum += i as f64;
+    }
+    println!("Sume is {}", sum)
+    //all variables must be created using the let keyword
+    //variables are by default read only, not mutable!!
+    //nonmut = 31;
+    //println!("sum is {}", nonmut);
+
+}
