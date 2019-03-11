c3d7dde1c2f57cb112944796712ea9cd1f5587dd
Rob Farrow <rob.farrow@hotmail.com>
2019-03-11 21:15:27 +0000

    learnt some more!

--- Hello_World.rs
+++ Hello_World.rs
@@ -1,7 +1,7 @@
 // Rust has to have a main function
 fn main() {
 
-    let answer = 43;
+    let answer :i32 = 43;
     println!("Hello {}", answer);
     assert_eq!(answer, 43);
     for i in 0..7 {
@@ -21,17 +21,25 @@ fn main() {
         println!("{} {}", even_odd, i);
     }
     println!("test {} {}", 1, 1);
-    println!("{1}, {0}", "alice", "bob");
+    println!("{1}, {0}", "jim", "dwight");
     //formats the number as a binary string
     println!("{} of {:b} people know binary, the other half doesn't", 1, 226);
-    let mut sum = 0.0;
-    for i in 0..47 {
+    let mut sum :f64 = 0.0f64;
+    for i in 0i32..47i32 {
         sum += i as f64;
     }
-    println!("Sume is {}", sum)
+    println!("Sum is {}", sum);
     //all variables must be created using the let keyword
     //variables are by default read only, not mutable!!
     //nonmut = 31;
     //println!("sum is {}", nonmut);
 
+    let res :f64 = sqr(2.87f64);
+    println!("{}", res);
+
+}
+
+//In functions, the compiler will not infer types
+fn sqr(x: f64) -> f64 {
+    return x * x;
 }
